5f3a12a94263f2405a4aeb4ea9351d1d056e52aa
Shao-yan (Matt) Ji <github@mail.shaoyanji@is-a.dev>
2026-03-24 23:47:09 +0100

    Add minimal web server skeleton

---
+++ internal/app/routes.go
@@ -0,0 +1,33 @@
+package app
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// NewRouter wires the minimal HTTP surface for milestone bootstrap.
+func NewRouter() (http.Handler, error) {
+	homeTemplate, err := template.ParseFiles("internal/views/home.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	r := chi.NewRouter()
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		if err := homeTemplate.Execute(w, nil); err != nil {
+			http.Error(w, "template render error", http.StatusInternalServerError)
+		}
+	})
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	return r, nil
+}
